b791faf99ad27aa96ad9e2c8f74dcaa9de0a2022
Shoutugh <m.alabazo23@gmail.com>
2024-04-06 15:07:50 +0800

    update

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>Laraville</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
 
@@ -14,8 +14,8 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
 
-    <nav x-data="{ isOpen: false }" class="bg-grey-100 px-10 py-5">
-        <div class="px-10 border mx-auto rounded-full">
+    <nav x-data="{ isOpen: false }" class="bg-grey-100 px-5 py-5 sticky top-0 z-50 ">
+        <div class="px-10 border mx-auto rounded-full bg-white">
             <div class="flex justify-between">
                 <!-- logo -->
                 <div>
@@ -25,21 +25,20 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M14 10l-2 1m0 0l-2-1m2 1v2.5M20 7l-2 1m2-1l-2-1m2 1v2.5M14 4l-2-1-2 1M4 7l2-1M4 7l2 1M4 7v2.5M12 21l-2-1m2 1l2-1m-2 1v-2.5M6 18l-2-1v-2.5M18 18l2-1v-2.5" />
                         </svg>
-                        <span class="font-semibold">Box</span>
+                        <span class="font-semibold">Laraville</span>
                     </a>
                 </div>
                 <!-- Primary Navigation -->
                 <div class="hidden md:flex items-center space-x-1 uppercase">
-                    <a class="py-5 px-2 text-gray-700 hover:text-gray-900 " href="#">Services</a>
-                    <a class="py-5 px-2 text-gray-700 hover:text-gray-900" href="#">Projects</a>
-                    <a class="py-5 px-2 text-gray-700 hover:text-gray-900" href="#">About</a>
-                    <a class="py-5 px-2 text-gray-700 hover:text-gray-900" href="#">Insights</a>
+                    <a class="py-5 px-2 text-inherit hover:text-gray-900 " href="#">Services</a>
+                    <a class="py-5 px-2 text-inherit hover:text-gray-900" href="#">Projects</a>
+                    <a class="py-5 px-2 text-inherit hover:text-gray-900" href="#">About</a>
+                    <a class="py-5 px-2 text-inherit hover:text-gray-900" href="#">Insights</a>
                 </div>
                 <!-- Secondary Navigation -->
                 <div class="hidden md:flex items-center space-x-1 uppercase">
-                    <a class="py-5 px-3 text-gray-800 hover:text-gray-900" href="#">Login</a>
                     <a class="py-2 px-3 bg-blue-700 hover:text-blue-100 text-blue-200 transition duration-300 rounded-full shadow"
-                        href="#">Signup</a>
+                        href="#">get in touch</a>
                 </div>
                 <!-- Mobile Menu -->
                 <div class="md:hidden flex items-center ">
@@ -52,15 +51,19 @@
                 </div>
             </div>
         </div>
-        <div x-show="isOpen" class="mobile-menu md:hidden uppercase py-5 ">
+        <div x-show="isOpen" class="mobile-menu md:hidden uppercase bg-white gap-2 rounded-2xl border my-3 py-2 text-center">
             <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Services</a>
+            <hr class="h-px my-2 bg-black">
             <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Projects</a>
+            <hr class="h-px my-2 bg-black">
             <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">About</a>
+            <hr class="h-px my-2 bg-black">
             <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Insights</a>
-            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Login</a>
-            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">Sign up</a>
+            <hr class="h-px my-2 bg-black">
+            <a href="#" class="block py-2 px-4 text-sm hover:bg-gray-200">GEt in touCh</a>
         </div>
     </nav>
 </head>
 
 <body>
+    
